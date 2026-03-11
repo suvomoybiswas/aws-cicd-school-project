ceb4dd34f1557c1aa7a784cd470212a3dd098dcc
Suvo Biswas <suvom8sau@gmail.com>
2026-03-11 03:40:22 +0000

    update message

--- app.py
+++ app.py
@@ -1,14 +1,1 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return "Hello from my CI/CD pipeline!"
-
-@app.route("/health")
-def health():
-    return "OK"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+return "Hello from my CI/CD pipeline v2!"
